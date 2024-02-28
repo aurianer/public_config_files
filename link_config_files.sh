821d00bc45fd146219ec4a44561207e2a9d08d2f
aurianer <8trash-can8@protonmail.ch>
2024-02-28 15:28:52 +0100

    Delete backups if identical to the original files

--- link_config_files.sh
+++ link_config_files.sh
@@ -20,6 +20,11 @@ function check_if_exist_and_link() {
         ln -s $cloned_dir/.$1 ~/.$1
         popd > /dev/null
     fi
+    # Compare the backup and the new file, deletes the backup if identical
+    diff -q ~/.$1 ~/.$1.bkp > /dev/null
+    if [[ $? == 0 ]]; then
+        rm ~/.$1.bkp
+    fi
 }
 
 cloned_dir=$PWD
